8d20711865e4bd7182850b68afb571b5223dd833
Martin Eden <martin.s.eden@gmail.com>
2017-07-07 11:12:03 +0100

    Expand default policy. Also remove dependency on registry.

--- include/start-vault.sh
+++ include/start-vault.sh
@@ -1,6 +1,8 @@
-image=docker.montagu.dide.ic.ac.uk:5000/montagu-vault:master
+#image=docker.montagu.dide.ic.ac.uk:5000/montagu-vault:master
+#docker pull $image
 
-docker pull $image
+image=montagu-vault
+docker build -t $image .
 
 docker run -d --rm \
     --cap-add=IPC_LOCK \
