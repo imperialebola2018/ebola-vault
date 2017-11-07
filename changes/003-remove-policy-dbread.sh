c3317a532d110b2f2d26dbc5a56b448fbdc1a218
Rich <r.fitzjohn@imperial.ac.uk>
2017-11-07 10:05:46 +0000

    Remove passwords at same time as revoking policy

--- changes/003-remove-policy-dbread.sh
+++ changes/003-remove-policy-dbread.sh
@@ -9,3 +9,10 @@ fi
 echo "Removing science read permissions from /secret/database"
 vault delete auth/github/map/teams/science
 vault policy-delete dbread
+
+USERS="api import orderly readonly schema_migrator"
+for U in $USERS; do
+    KEY="secret/database/users/$U"
+    echo "Removing password $KEY"
+    vault delete $KEY
+done
