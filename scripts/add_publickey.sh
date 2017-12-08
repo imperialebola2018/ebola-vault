9a8d990769f3bb9acab2d7931d951777224de79a
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2017-12-08 14:05:12 +0000

    Script for adding an ssl public key

---
+++ scripts/add_publickey.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -e
+if [ "$#" -ne 1 ]; then
+    echo "Usage:\n    $0 <name>"
+    exit 1
+fi
+
+NAME=$1
+DEST="pubkey/ssl/${NAME}.pub"
+SRC=$(readlink -f ~/.ssh/id_rsa)
+
+echo "Converting ssh key $SRC into public key $DEST"
+
+mkdir -p pubkey/ssl
+openssl req -x509 -new -key $SRC -days 3650 -nodes \
+        -subj "/C=US/ST=*/L=*/O=*/OU=*/CN=Carol/" -out $DEST
+
+echo "Success!"
+echo
+echo "Please add $DEST to git and commit"
