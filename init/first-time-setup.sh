b026d2f8538c83d86e7910edefb33aacfd5baea3
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2018-05-18 17:55:08 +0100

    Use correct names

--- init/first-time-setup.sh
+++ init/first-time-setup.sh
@@ -8,11 +8,11 @@ HERE=$(dirname $0)
 
 vault auth $TOKEN
 vault auth-enable github
-vault write auth/github/config organization=ebolas2018
+vault write auth/github/config organization=imperialebola2018
 vault write auth/github/map/teams/core value=standard
 vault policy-write standard "$HERE/../config/standard.policy"
 
-echo "Enabled GitHub authentication for the core team in the vimc organization"
+echo "Enabled GitHub authentication for the core team in the imperialebola2018 organization"
 echo "You can now authenticate against the vault using"
 echo "    vault login -method=github token=[GITHUB PERSONAL ACCESS TOKEN]"
 echo "To obtain your GitHub personal access token see https://help.github.com/articles/creating-an-access-token-for-command-line-use/"
